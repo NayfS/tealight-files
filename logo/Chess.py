132e51f546c353783c174e8e05773184b0c92cd7
NayfS <nathan.sealey@outlook.com>
2015-08-18 11:55:38 +0100

    Running logo/Chess.py

--- logo/Chess.py
+++ logo/Chess.py
@@ -6,14 +6,14 @@ def polygon(edges, size):
     move(size)
     turn(angle)
     
-polygon(4,100)
+
 
 def board(edges, size):
   angle = 360/ edges
   decoration = size / 1
   for i in range(0, edges):
     move(size)
-    polygon(decoration)
+    polygon(4,100)
     turn(angle)
 
 turn(-90)
