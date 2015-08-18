8c694aa3530e105cd448436e8cdc90577c7a648d
NayfS <nathan.sealey@outlook.com>
2015-08-18 12:21:14 +0100

    Running logo/Chess.py

--- logo/Chess.py
+++ logo/Chess.py
@@ -6,22 +6,5 @@ def square(edges, size):
       move(size)
       turn(angle)
       move(size)
-    turn(90)
-    for i in range(0,2):
-      for i in range(0, edges):
-       move(size)
-       turn(angle)
-       move(size)
-    turn(90)
-    for i in range(0,8):
-      for i in range(0, edges):
-       move(size)
-       turn(angle)
-       move(size)
-    turn(180)
-    for i in range(0,8):
-     for i in range(0, edges):
-       move(size)
-       turn(angle)
-       move(size)
-square(4,50)
+  turn(90)
+square(4,10)
