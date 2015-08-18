dd39687d99f81d82792dd9e0098a212ea7b0a26b
NayfS <nathan.sealey@outlook.com>
2015-08-18 12:14:16 +0100

    Running logo/Chess.py

--- logo/Chess.py
+++ logo/Chess.py
@@ -19,7 +19,7 @@ def square(edges, size):
           turn(angle)
         move(size)
   turn(180)
-  for i in range(0,2):
+  for i in range(0,8):
       for i in range(0, edges):
         move(size)
         turn(angle)
