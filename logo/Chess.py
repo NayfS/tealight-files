e76ff0b56131e3bb199b6306738c862e98344f82
NayfS <nathan.sealey@outlook.com>
2015-08-18 12:22:44 +0100

    Running logo/Chess.py

--- logo/Chess.py
+++ logo/Chess.py
@@ -1,8 +1,7 @@
 from tealight.logo import move, turn
 def square(edges, size):
   angle = 360.0 / edges
-  for i in range(0,2):
-    for i in range(0, edges):
+  for i in range(0, edges):
       move(size)
       turn(angle)
   turn(90)
