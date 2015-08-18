52599a37881d076915723ca859c1b0b0b5640d7f
NayfS <nathan.sealey@outlook.com>
2015-08-18 15:09:31 +0100

    Running robot/field.py

--- robot/field.py
+++ robot/field.py
@@ -6,26 +6,18 @@ from tealight.robot import (move,
                             left_side, 
                             right_side)
 
-i=0
-j=1
 while True:
-  if right_side()=='fruit':
+  if look()=='fruit':
+    move()
+  elif right_side()=='fruit':
     turn(1)
-    move()
-    i=0
+    while look()=='fruit':
+      move()
   elif left_side()=='fruit':
-    turn(2)
-    move()
-    i=0
-  elif look()=='fruit':
-    move()
-    i=0
-  if i>3:
-    i=0
-    for j in (0,5):
-      move
+    turn(1)
+    while look()=='fruit':
+      move()
   else:
     turn(1)
     move()
-    i=i+1
     
