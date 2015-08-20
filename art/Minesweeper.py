4da6e7809be4f2db5544112cb3c190dc6a8d098d
NayfS <nathan.sealey@outlook.com>
2015-08-20 14:35:35 +0100

    Running art/Minesweeper.py

--- art/Minesweeper.py
+++ art/Minesweeper.py
@@ -61,8 +61,6 @@ def handle_mousedown(x, y,button):
           color("black")
           text(0, 600,"Final Score: "+str(score))
           text(500,600, "You Lost!")
-          while True:
-            print ""
         if get(mine, boxX, boxY)==0:
           color('white')
           box(boxX*60,boxY*60,50,50)
