a8ec2a4c2bffddbf04d0984c7aaaf2f862d14319
NayfS <nathan.sealey@outlook.com>
2015-08-20 10:43:05 +0100

    Running art/Minesweeper.py

--- art/Minesweeper.py
+++ art/Minesweeper.py
@@ -5,11 +5,12 @@ list=[]
 
 for j in range(0,10):
   for i in range(0,10):
-    box(i*50,j*50,40,40)
+    y=box(i*50,j*50,40,40)
     
 for i in range (0,15):
   x=random.randrange(1,100,1)
   y=random.randrange(1,100,1)
   list.append((x,y))
 
+ycolor('bblue)
 print list
