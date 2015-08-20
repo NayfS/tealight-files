f8dc12017e8550762a3c02971806d8d22997fef5
NayfS <nathan.sealey@outlook.com>
2015-08-20 10:28:02 +0100

    Running art/Minesweeper.py

--- art/Minesweeper.py
+++ art/Minesweeper.py
@@ -1,8 +1,8 @@
 from tealight.art import (color, line, spot, circle, box, image, text, background)
 import random
 from random import randint
-
+x=0
 for j in range(0,10):
   for i in range(0,10):
-    i=box(i*50,j*50,40,40)
+    x+1=box(i*50,j*50,40,40)
     
