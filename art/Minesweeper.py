8ee2dac58bd8ed94268995abc21e863fb5c1f20b
NayfS <nathan.sealey@outlook.com>
2015-08-20 10:49:48 +0100

    Running art/Minesweeper.py

--- art/Minesweeper.py
+++ art/Minesweeper.py
@@ -1,7 +1,13 @@
 from tealight.art import (color, line, spot, circle, box, image, text, background)
 import random
 from random import randint
+import github.fourthdwarflord.art.Minesweeper.py
+
 list=[]
+
+for i in range (0, 100):
+  list.append(0)
+
 
 for j in range(0,10):
   for i in range(0,10):
@@ -10,7 +16,6 @@ for j in range(0,10):
 for i in range (0,15):
   x=random.randrange(1,100,1)
   y=random.randrange(1,100,1)
-  list.append((x,y))
-
+  setbox(list, x, y, 1)
 
 print list
