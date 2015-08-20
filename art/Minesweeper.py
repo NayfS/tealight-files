a73ba5e8a956fa3e25ec2cc5f6aa15d36a95a6ce
NayfS <nathan.sealey@outlook.com>
2015-08-20 10:26:12 +0100

    Running art/Minesweeper.py

--- art/Minesweeper.py
+++ art/Minesweeper.py
@@ -4,5 +4,6 @@ from random import randint
 
 for j in range(0,10):
   for i in range(0,10):
+    color(random.choice( ['red','red', 'blue', 'green'] ))
     box(i*50,j*50,40,40)
-    color(random.choice( ['red', 'blue', 'green'] ))
+    
