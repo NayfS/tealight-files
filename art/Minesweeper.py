e7ef887b5f86ed8644aafe43f830e3ddc386b63f
NayfS <nathan.sealey@outlook.com>
2015-08-20 14:00:14 +0100

    Running art/Minesweeper.py

--- art/Minesweeper.py
+++ art/Minesweeper.py
@@ -54,8 +54,10 @@ def handle_mousedown(x, y):
     box(boxX*60,boxY*60,50,50)
     print getSurroundingMines(boxX,boxY)
     score+=1
+    color("white")
     box(0,600,500,50)
-    text(0, 600,score)
+    color("black")
+    text(0, 600,"Score: "+str(score))
 
 color('black')
 
