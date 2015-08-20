c452a6b392946cb2ccd085c3dcf239cc388910f2
NayfS <nathan.sealey@outlook.com>
2015-08-20 10:20:14 +0100

    Running art/Minesweeper.py

--- art/Minesweeper.py
+++ art/Minesweeper.py
@@ -1,6 +1,6 @@
 from tealight.art import (color, line, spot, circle, box, image, text, background)
 
-
-for i in range(0,10):
-  box(i*60,j*10,50,50)
-  
+for j in range(0,10):
+  for i in range(0,10):
+    box(i*60,j*10,50,50)
+    
